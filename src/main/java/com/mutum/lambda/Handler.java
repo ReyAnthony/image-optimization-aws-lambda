fc85180b21d5a6f2ee4bd612accae34d6496b432
Anthony REY anthony@mutum.fr <anthony.rey5@gmail.com>
2016-11-22 16:06:10 +0100

    Set target to java 1.8 and removed unused imports

--- src/main/java/com/mutum/lambda/Handler.java
+++ src/main/java/com/mutum/lambda/Handler.java
@@ -1,6 +1,5 @@
 package com.mutum.lambda;
 
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
